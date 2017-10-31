12db74778a84f7cac808b52aec2b06c641de98f6
Shoumik Jamil <shoumik@airbears2-10-142-112-115.airbears2.1918.berkeley.edu>
2017-10-31 12:19:01 -0700

    Fixed anchor link issue in Chrome

--- js/custom.js
+++ js/custom.js
@@ -103,6 +103,20 @@ $(function(){
         ],  {duration: 2000, fade: 750});
     });
   })
+$(function() {
+       $('a[href*="#"]:not([href="#"])').click(function() {
+         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+           var target = $(this.hash);
+           target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html, body').animate({
+               scrollTop: target.offset().top
+             }, 1000);
+             return false;
+           }
+         }
+       });
+     });
 
 });
 
